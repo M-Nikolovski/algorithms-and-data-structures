fdc2a244b58c5d9293fb88817ab8a0c71f89ad8c
Mirko Nikolovski <nikolovski-m@hotmail.com>
2019-09-09 18:55:58 +0200

    - Added ListaOdListi solution

---
+++ exercises/1st-midterm/ListaOdListi.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+class DLLNode<E> {
+    protected E element;
+    protected DLLNode<E> pred, succ;
+
+    public DLLNode(E elem, DLLNode<E> pred, DLLNode<E> succ) {
+        this.element = elem;
+        this.pred = pred;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return "<-" + element.toString() + "->";
+    }
+}
+
+class DLL<E> {
+    private DLLNode<E> first, last;
+
+    public DLL() {
+        // Construct an empty SLL
+        this.first = null;
+        this.last = null;
+    }
+
+    public void insertFirst(E o) {
+        DLLNode<E> ins = new DLLNode<E>(o, null, first);
+        if (first == null)
+            last = ins;
+        else
+            first.pred = ins;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first == null)
+            insertFirst(o);
+        else {
+            DLLNode<E> ins = new DLLNode<E>(o, last, null);
+            last.succ = ins;
+            last = ins;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            ret += tmp + "<->";
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += tmp + "<->";
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public DLLNode<E> getFirst() {
+        return first;
+    }
+}
+
+public class ListaOdListi {
+
+    public static long findMagicNumber(DLL<DLL<Integer>> list) {
+        //Vashiot kod tuka...
+
+        DLLNode<DLL<Integer>> listIterator = list.getFirst();
+        DLLNode<Integer> insideListIterator;
+        long product = 1;
+        while (listIterator != null) {
+            insideListIterator = listIterator.element.getFirst();
+            int sum = 0;
+            while (insideListIterator != null) {
+                sum += insideListIterator.element;
+                insideListIterator = insideListIterator.succ;
+            }
+            product *= sum;
+            listIterator = listIterator.succ;
+        }
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        DLL<DLL<Integer>> list = new DLL<DLL<Integer>>();
+        for (int i = 0; i < n; i++) {
+            DLL<Integer> tmp = new DLL<Integer>();
+            for (int j = 0; j < m; j++) {
+                tmp.insertLast(in.nextInt());
+            }
+            list.insertLast(tmp);
+        }
+        in.close();
+        System.out.println(findMagicNumber(list));
+    }
+}
